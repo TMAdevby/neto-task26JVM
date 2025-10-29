fb28520a8d2d7535df8eb371acd9171fc0224177
TMAdevby <mtipun@mail.ru>
2025-10-29 19:59:28 +0300

    jvm5

--- src/main/java/ru/netology/JvmComprehension.java
+++ src/main/java/ru/netology/JvmComprehension.java
@@ -8,7 +8,11 @@ public class JvmComprehension {
         Object o = new Object();        // 2 В heap создаётся новый экземпляр java.lang.Object
                                         //В стеке (фрейм main) создаётся ссылка o, указывающая на этот объект в heap
                                         //Класс Object уже загружен → его метаданные в Metaspace
-        Integer ii = 2;                 // 3
+        Integer ii = 2;                 // 3 Создаётся новый стековый фрейм для метода printAll
+                                        //В него копируются значения аргументов:
+                                        //Ссылка o (копия указателя на тот же Object)
+                                        //Значение i = 1 (примитив — копируется по значению)
+                                         //Ссылка ii (копия указателя на Integer(2))
         printAll(o, i, ii);             // 4
         System.out.println("finished"); // 7
     }
